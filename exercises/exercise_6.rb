365768a0348a9bfff6ae451c5a7e4923253e03be
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-13 20:10:43 -0600

    add logic to example 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -14,3 +14,63 @@ puts "----------"
   last_name: "Virani",
   hourly_rate: 60
 )
+@store1.employees.create(
+  first_name: "Kourtney",
+  last_name: "Act",
+  hourly_rate: 15
+)
+@store1.employees.create(
+  first_name: "Adore",
+  last_name: "Delano",
+  hourly_rate: 15
+)
+@store1.employees.create(
+  first_name: "Bianca",
+  last_name: "Del Rio",
+  hourly_rate: 40
+)
+@store2.employees.create(
+  first_name: "Darrien",
+  last_name: "Lake",
+  hourly_rate: 20
+)
+@store2.employees.create(
+  first_name: "Violet",
+  last_name: "Chachki",
+  hourly_rate: 20
+)
+@store2.employees.create(
+  first_name: "Katya",
+  last_name: "Vlad",
+  hourly_rate: 20
+)
+@store2.employees.create(
+  first_name: "Simone",
+  last_name: "Byers",
+  hourly_rate: 20
+)
+@store2.employees.create(
+  first_name: "Gigi",
+  last_name: "Good",
+  hourly_rate: 20
+)
+@store2.employees.create(
+  first_name: "Captain",
+  last_name: "America",
+  hourly_rate: 15
+)
+@store2.employees.create(
+  first_name: "Tony",
+  last_name: "Stark",
+  hourly_rate: 15
+)
+@store2.employees.create(
+  first_name: "Clark",
+  last_name: "Kent",
+  hourly_rate: 15
+)
+@store2.employees.create(
+  first_name: "Trixie",
+  last_name: "Mattel",
+  hourly_rate: 600
+)
